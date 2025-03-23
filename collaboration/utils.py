9d5c112b9675d1cc2a78f2256867d605aad78cd4
Strelixia <maguy.birikomo@gmail.com>
2025-03-23 18:07:02 +0100

    Fonction pour l'Envoi des emails

--- collaboration/utils.py
+++ collaboration/utils.py
@@ -1,10 +1,10 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
-def send_email(owner, collaborator, subject, template_name, reset_url):               
+def send_email(owner, collaborator, subject, template_name, inviting_url):               
     email_html_content = render_to_string(template_name, {
-        'owner': owner.username,
-        'reset_url': reset_url
+        'owner': owner.email,
+        'inviting_url': inviting_url
     })
 
     email = EmailMessage(
@@ -16,3 +16,4 @@ def send_email(owner, collaborator, subject, template_name, reset_url):
 
     email.content_subtype = "html"
     email.send()
+    
